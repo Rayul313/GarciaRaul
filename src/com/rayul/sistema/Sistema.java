ad10a8f94c9770034973ee473f3f9f82daa058f2
Raul Garcia <Raul Garcia@192.168.1.142>
2020-06-14 20:57:58 +0200

    Commit no.1 (agregar la clase Sistema)

---
+++ src/com/rayul/sistema/Sistema.java
@@ -0,0 +1,14 @@
+package com.rayul.sistema;
+
+public class Sistema {
+
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+
+	}
+
+}
